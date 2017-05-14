83cda8aa1ee727e5dcfbeb6021d29b6af931a781
jc321013 <jared.marcolongo@my.jcu.edu.au>
2017-05-14 21:36:05 +1000

    Initial commit

---
+++ app/src/main/java/com/example/jc321013/test/Database.java
@@ -0,0 +1,32 @@
+package com.example.jc321013.test;
+
+/**
+ * Created by jc321013 on 14/05/2017.
+ */
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by jc321013 on 14/05/2017.
+ */
+
+public class Database extends SQLiteOpenHelper {
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    public Database(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
